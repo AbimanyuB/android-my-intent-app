19d0a4c54de9322c15bee09041f940156caa17d8
AbimanyuB <abimanyu.bhamakerti@gmail.com>
2020-07-02 18:03:09 +0700

    Initial Commit

---
+++ app/src/main/java/com/e/myintentapp/MainActivity.java
@@ -0,0 +1,49 @@
+package com.e.myintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMoveActivity = findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener(this);
+
+        Button btnMoveWithDataActiviry = findViewById(R.id.btn_move_activity_data);
+        btnMoveWithDataActiviry.setOnClickListener(this);
+
+        Button btnDialPhone = findViewById(R.id.btn_dial_number);
+        btnDialPhone.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_move_activity:
+                Intent moveActivity =  new Intent(MainActivity.this, MoveActivity.class);
+                startActivity(moveActivity);
+                break;
+            case  R.id.btn_move_activity_data:
+                Intent moveWithDataIntent = new Intent(MainActivity.this,
+                        MoveWithDataActivity.class);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Abimanyu Bhamakerti");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 22);
+                startActivity(moveWithDataIntent);
+                break;
+            case R.id.btn_dial_number:
+                String phoneNumber = "081212121212";
+                Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+phoneNumber));
+                startActivity(dialPhoneIntent);
+                break;
+        }
+    }
+}
